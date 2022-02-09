185a44fd46ea38b16b74ec1f752b6b81f80d0acf
Anne <anne.mboke@epitech.eu>
2022-02-09 20:57:27 +0100

    [ADD] Emile page for the portfolio

---
+++ src/Emile.js
@@ -0,0 +1,16 @@
+function Emile() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1>Emile</h1>
+        </header>
+        <body>
+          <h2>A great Activist</h2>
+          <h3>Thank you for this workshop. It was really fun !</h3>
+          <h4>Don't forget that wa have a hackathon to organize :) (signed Anne)</h4>
+          </body>
+      </div>
+    );
+  }
+  
+  export default Emile;
