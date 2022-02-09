52874382f0278403536d1ca0b909de84d8ccd88c
Luca <luca.paulekat@epitech.eu>
2022-02-09 20:56:52 +0100

    This commit adds the Luca page for the portfolio

---
+++ portfolio-group/src/Luca.js
@@ -0,0 +1,20 @@
+function Luca() {
+    return (
+        <div>
+            <h1>
+            <span>
+                Luca<br />
+                Paulekat
+            </span>
+                <br />
+            </h1>
+            <span>Student at Epitech Berlin</span>
+            <p>Germany, Berlin</p>
+            <a href="mailto:lpaulekat@yahoo.de" target="_blank">lpaulekat@yahoo.de</a>
+            <h3>Profile:</h3>
+            <p>Idk what to say on here for the Workshop I am very sorry :/.</p>
+        </div>
+    );
+}
+
+export default Luca;
